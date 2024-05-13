83933c54666872e50a114358538ad8b27c6633ae
Harshiiet Negi <negeek7@gmail.com>
2024-05-14 02:29:22 +0530

    country card template

---
+++ src/components/CountryCard.tsx
@@ -0,0 +1,27 @@
+import styles from '../../styles/countrycard.module.css'
+
+function CountryCard() {
+    return (
+        <div className={`${styles.container}`}>
+            <div className={styles.imagecontainer}>
+                <img src="https://flagcdn.com/in.svg" />
+            </div>
+            <div className={styles.bottomcontainer}>
+                <p className="font-semibold text-xl mt-2 mb-4">India</p>
+                <div className="flex flex-col gap-2">
+                    <div>
+                        <span className="font-semibold">Population:</span> <span>27,657,125</span>
+                    </div>
+                    <div>
+                        <span className="font-semibold">Region:</span> <span>Asia</span>
+                    </div>
+                    <div>
+                        <span className="font-semibold">Capital:</span> <span>Delhi</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CountryCard
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import CountryCard from "./CountryCard"
+
+function Home() {
+  return (
+    <div className="flex flex-row flex-wrap border-2 h-dvh p-14">
+        <CountryCard />
+    </div>
+  )
+}
+
+export default Home
